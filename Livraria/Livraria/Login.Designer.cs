68e5a49bd36159ad8c3f9ad1103a6bd9d56019b9
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-06-24 11:42:00 -0300

    Add project files.

---
+++ Livraria/Livraria/Login.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Livraria
+{
+    partial class Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            btnFechar = new Button();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.GradientInactiveCaption;
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(300, 491);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(btnFechar);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(300, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(434, 491);
+            panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe Print", 27F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(3, 218);
+            label1.Name = "label1";
+            label1.Size = new Size(298, 64);
+            label1.TabIndex = 0;
+            label1.Text = "Livraria Online";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15F, FontStyle.Italic);
+            label2.Location = new Point(22, 185);
+            label2.Name = "label2";
+            label2.Size = new Size(145, 28);
+            label2.TabIndex = 1;
+            label2.Text = "Bem-vindo(a) à";
+            // 
+            // btnFechar
+            // 
+            btnFechar.BackColor = Color.Maroon;
+            btnFechar.Cursor = Cursors.Hand;
+            btnFechar.FlatAppearance.BorderSize = 0;
+            btnFechar.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            btnFechar.ForeColor = Color.White;
+            btnFechar.Location = new Point(394, 0);
+            btnFechar.Name = "btnFechar";
+            btnFechar.Size = new Size(40, 40);
+            btnFechar.TabIndex = 0;
+            btnFechar.Text = "X";
+            btnFechar.UseVisualStyleBackColor = false;
+            btnFechar.Click += button1_Click;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(734, 491);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Login";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Panel panel2;
+        private Label label2;
+        private Button btnFechar;
+    }
+}
---
+++ Livraria/Livraria/Login.cs
@@ -0,0 +1,20 @@
+namespace Livraria
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
