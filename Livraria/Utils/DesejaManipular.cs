cd8e07baeed322d976025c69f7aa080f866ca510
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-06-28 20:13:22 -0300

    tornará o método dinâmico

--- Livraria/Utils/DesejaManipular.cs
+++ Livraria/Utils/DesejaManipular.cs
@@ -3,12 +3,12 @@ using System.Windows.Forms;
 
 namespace Livraria.Utils
 {
-    public static class DesejaSair
+    public static class DesejaManipular
     {
-        public static DialogResult Confirmar()
+        public static DialogResult ConfirmarAcao(string contexto)
         {
             return MessageBox.Show(
-                "Tem certeza que deseja sair?",
+                $"Tem certeza que deseja {contexto}?",
                 "Atenção!!!",
                 MessageBoxButtons.YesNo,
                 MessageBoxIcon.Exclamation
